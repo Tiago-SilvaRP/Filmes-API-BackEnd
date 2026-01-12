964dbd8aacd4404d76c685c67d55affd89208d7d
unknown <tiagosilva_live_2016@outlook.com>
2026-01-12 10:02:53 -0300

    config express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-const name = "tiago";
+import express from "express";
+const port = 3000;
+const app = express();
+
+app.get("/movies", (req, res) => {
+  res.send("Listagem de filmes");
+});
+
+app.use((error, req, res, next) => {
+  console.error(`Erro apresentado: ${error.stack}`);
+  res.status(500).send("Algo deu errado");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
